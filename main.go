7fd2fe8959dc23e9f5b3a8e6aacff7652add6bed
ashiful islam <ashifulislam2002@gmail.com>
2026-05-10 23:56:29 +0600

    learning functions

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sugar := 2
+
+	// Anonymous function
+	coffeeBanao := func() {
+		fmt.Println("Making coffee...")
+	}
+	coffeeBanao()
+
+	// IIFE (Immediately Invoked Function Expression)
+	func() {
+		fmt.Printf("Making %d tea...\n", sugar)
+	}()
+
+	// IIFE with parameters
+	func(beverage string) {
+		fmt.Printf("Making %s...\n", beverage)
+	}("juice")
+
+	makeChai := func() {
+		chai := "Black tea"
+		fmt.Printf("Making... %s with %d spoons of sugar\n", chai, sugar)
+	}
+	makeChai()
+}
