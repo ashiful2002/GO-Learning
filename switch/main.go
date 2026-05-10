eb39c542e31cc43f91700e39949fd2263c3a8b9e
ashiful islam <ashifulislam2002@gmail.com>
2026-05-11 00:35:12 +0600

    exploreing switch case

---
+++ switch/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	day := "Friday"
+
+	switch day {
+	case "Monday":
+		fmt.Printf("%s It's the start of the week.\n", day)
+	case "Tuesday":
+		fmt.Printf("%s It's the second day of the week.\n", day)
+	case "Wednesday":
+		fmt.Printf("%s It's the middle of the week.\n", day)
+	case "Thursday":
+		fmt.Printf("%s It's almost the weekend.\n", day)
+	case "Friday":
+		fmt.Printf("%s It's the last day of the workweek.\n", day)
+	case "Saturday", "Sunday":
+		fmt.Printf("%s It's the weekend!\n", day)
+	default:
+		fmt.Println("Invalid day.")
+	}
+
+	
+}
